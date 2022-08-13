3f5376a18d6c68d782716ed26da7b9f56ddcb170
Alexey <alexlisenkov92@mail.ru>
2022-08-13 12:27:03 +0300

    add test add local selenoid address config

--- src/test/java/com/alexlis/tests/TestBase.java
+++ src/test/java/com/alexlis/tests/TestBase.java
@@ -8,7 +8,6 @@ import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.openqa.selenium.remote.DesiredCapabilities;
 import static com.codeborne.selenide.Selenide.closeWebDriver;
-import static java.lang.String.format;
 
 public class TestBase {
 
